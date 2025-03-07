e882590982c114578e88ae60972fcd47e3b6545c
Sebastian Kliś <klis.seb@gmail.com>
2025-03-07 23:05:34 +0100

    adjusted navigation transitions

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ const navMobile = document.querySelector('.nav-mobile');
 const handleNav = () => {
 	navMobile.classList.toggle('nav-active');
 	burgerBtn.classList.toggle('burger-active');
+	navMobile.style.visibility = 'visible';
 };
 
 burgerBtn.addEventListener('click', handleNav);
