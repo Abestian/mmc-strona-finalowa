8f5347b9b1695c92cd86a5d6991d0fb2369cb92a
Sebastian Kliś <klis.seb@gmail.com>
2025-03-08 23:10:58 +0100

    Added vertical animation to eyecatcher's text

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import './sass/main.scss';
 
 const burgerBtn = document.querySelector('.burger-btn');
 const navMobile = document.querySelector('.nav-mobile');
+const eyecatcher = document.querySelector('.eyecatcher');
+const movingText = document.querySelector('.eyecatcher__moving-text');
 
 const handleNav = () => {
 	navMobile.classList.toggle('nav-active');
@@ -11,4 +13,20 @@ const handleNav = () => {
 	navMobile.style.visibility = 'visible';
 };
 
+function isInViewport(item) {
+	const bounding = item.getBoundingClientRect();
+	return (
+		bounding.top >= -item.offsetHeight &&
+		bounding.left >= -item.offsetWidth &&
+		bounding.right <= window.innerWidth + item.offsetWidth &&
+		bounding.bottom <= window.innerHeight + item.offsetHeight
+	);
+}
+
 burgerBtn.addEventListener('click', handleNav);
+
+window.addEventListener('scroll', function () {
+	if (isInViewport(eyecatcher)) {
+		movingText.classList.add('animation');
+	}
+});
